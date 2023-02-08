39293eded1f6d9be6d983d7273475604d6137552
Joyiieee <123354370+Joyiieee@users.noreply.github.com>
2023-02-08 20:38:20 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+var greet = require ('./message');
+
+console.log(greet.greet('Clarence'));
+
+var congratulations = require ('./message');
+
+console.log(congratulations.congratulations('Clarence'));
+
+var farewell = require ('./message');
+
+console.log(farewell.farewell('Clarence'));
---
+++ message.js
@@ -0,0 +1,14 @@
+module.exports.greet = function (name) {
+  
+    console.log('Hello ' + name)
+  };
+
+  module.exports.congratulations = function (name) {
+  
+    console.log('Congratulations ' + name)
+  };
+
+  module.exports.farewell = function (name){
+  
+    console.log('Farewell ' + name)
+  };
